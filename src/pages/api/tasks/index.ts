ca2efdaae20766789f935332d41f014dd5637adb
Charlie Mendoza <56177445+Gutsnt@users.noreply.github.com>
2023-01-30 13:13:02 -0400

    avance

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -7,8 +7,28 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 dbConnect();
 
 export default async function  handler(req: NextApiRequest,res: NextApiResponse){
-  const task = await Task.find()
-  console.log(task)
+  const {method, body} = req
 
-  res.status(200).json("tasks")
+  switch(req.method){
+
+    case 'GET':
+
+      try {
+      const task = await Task.find()
+      console.log(req.method,req.url);
+      return res.status(200).json(task);
+    } catch (error) {
+      return res.status(500).json({error: error.message})
+    }
+    
+    case 'POST':
+      const newTask = new Task(body)
+      const savedTask = await newTask.save()
+      return res.status(201).json(savedTask)
+
+    default:
+    return res.status(400).json({msg: 'Este metodo no es soportado'})
+
+  }
+  
 }
